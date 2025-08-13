d981903bc5d970e61ed8d07d40cc0efc21287a27
RubenSab <rubensabatini2005@gmail.com>
2025-08-13 13:12:54 +0200

    added things

--- model/player/map.py
+++ model/player/map.py
@@ -1,5 +1,7 @@
 MAP_HEIGHT = 20
-MAP_WIDTH = int(MAP_HEIGHT*3)
+ASPECT_RATIO = 2.5
+MAP_WIDTH = int(MAP_HEIGHT*ASPECT_RATIO)
+HIGHLIGHT_CHAR = '◇'
 
 class Map:
     def __init__(self):
@@ -7,15 +9,29 @@ class Map:
             'symbol': ' ',
             'note': ' '
         } for _ in range(MAP_WIDTH)] for __ in range(MAP_HEIGHT)]
+        self.highlighted_cell = None
+
+    def highlight_cell(self, x, y):
+        self.highlighted_cell = (x, y)
 
     def __str__(self):
         lines = '\n'.join([
-            '│'+''.join([i['symbol'] for i in row])+'│'
-            for row in self.matrix
+            '│' + ''.join(
+                [HIGHLIGHT_CHAR if (c, r) == self.highlighted_cell else cell['symbol']
+                 for c, cell in enumerate(row)]
+            ) + '│'
+            for r, row in enumerate(self.matrix)
         ])
-        upper_frame = '┌'+'─'*MAP_WIDTH+'┐\n'
-        lower_frame = '\n└'+'─'*MAP_WIDTH+'┘'
-        return upper_frame + lines + lower_frame
+        upper_frame = '┌' + '─' * MAP_WIDTH + '┐\n'
+        lower_frame = '\n└' + '─' * MAP_WIDTH + '┘\n'
+        if self.highlighted_cell is not None:
+            cell = self.get_cell(*self.highlighted_cell)
+            symbol = cell["symbol"] if cell["symbol"] != ' ' else HIGHLIGHT_CHAR
+            note = cell["note"] if cell["note"] != ' ' else 'No description'
+            note = f'[{symbol}] {note}'
+        else:
+            note = '[ ] Highlight position to view description'
+        return upper_frame + lines + lower_frame + note
 
     def mark_cell(self, x, y, char, note):
         if 0 <= x < MAP_WIDTH and 0 <= y < MAP_HEIGHT and len(str(char)) == 1:
